8df3d5ded33b3f4ab986b50e18d63c42c0083da9
Cody Holmes <57185912+codyaholmes@users.noreply.github.com>
2021-02-26 15:22:51 -0600

    Update storm_data.py

--- storm_data.py
+++ storm_data.py
@@ -5,20 +5,27 @@ import os
 
 # Prep the responses
 base_url = 'http://api.eia.gov/series/?series_id='
+
+# Dict holds desired col names and series id for response calls
 series_dict = {
     0: ['nat_gas', 'EBA.TEX-ALL.NG.NG.HL'],
     1: ['wind', 'EBA.TEX-ALL.NG.WND.HL'],
     2: ['coal', 'EBA.TEX-ALL.NG.COL.HL'],
     3: ['solar', 'EBA.TEX-ALL.NG.SUN.HL'],
+    4: ['hydro', 'EBA.TEX-ALL.NG.WAT.HL'],
+    5: ['nuclear', 'EBA.TEX-ALL.NG.NUC.HL'],
 }
 
 # Personal key can be registerd at: https://www.eia.gov/opendata/register.php
 api_key = os.getenv('EIA_API_KEY')
 
 def produce_dataset(series_dict):
+    """Preps energy source datasets then joins them all in one."""
+    
     # Empty list for dataframe concatenation
     dfs = []
     
+    # Loop through the responses and secure data for each energy source
     for i in range(len(series_dict)):
         response = requests.get('{}{}&api_key={}'.format(base_url, series_dict[i][1], api_key))
         data = response.json()
@@ -27,11 +34,12 @@ def produce_dataset(series_dict):
     
     # Combine all the energy source datasets into one
     final_df = pd.concat(dfs, axis=1)
+    final_df.reset_index(inplace=True)
+    final_df.rename(columns={'index': 'timestamp'}, inplace=True)
     
     return final_df
     
 data = produce_dataset(series_dict)
-data.reset_index(inplace=True)
 
 # Create separate date and time columns and drop timestamp
 data['date'] = data.timestamp.apply(lambda x: f'{x[4:6]}-{x[6:8]}-{x[0:4]}')
